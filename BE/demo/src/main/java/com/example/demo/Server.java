322da775b1e716a6cf9087d691a052dea1349eb4
chh303 <hjortlandc@gmail.com>
2025-02-05 21:00:45 +0100

    Server funksjon (CH)
    Kjør Server.java og prøv lenken under. (ved senere implementering gis denne nytt navn "test") http://localhost:9090/index.html

--- BE/demo/src/main/java/com/example/demo/Server.java
+++ BE/demo/src/main/java/com/example/demo/Server.java
@@ -12,12 +12,11 @@ public class Server {
 
 	@GetMapping("/api/hello")
 	public String sayHello(@RequestParam(value = "name", defaultValue = "verden") String name) {
-		return "Hei, " + name + "! Serveren fungerer.";
+		return "Hei, " + name + "! Serveren fungerer." + "\n" + "(Dette er en respons fra Server klassen.)";
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Server.class, args);
 	}
 	
-
 }
