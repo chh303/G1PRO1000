2ff09a481e6dde7b6aa5e7115875a09427abd5ea
chh303 <hjortlandc@gmail.com>
2025-03-19 16:17:56 +0100

    :D

--- BE/demo/src/main/java/com/g1pro1000/greenCode/repository/UserRepository.java
+++ BE/demo/src/main/java/com/g1pro1000/greenCode/repository/UserRepository.java
@@ -1,14 +1,17 @@
 package com.g1pro1000.greenCode.repository; // Angir hvilken pakke denne filen tilhører
 
-import org.springframework.data.jpa.repository.JpaRepository; // Importerer JpaRepository for å håndtere databaseoperasjoner
-import com.g1pro1000.greenCode.model.User; // Importerer User-modellen som representerer en rad i databasen
-import java.util.Optional; // Importerer Optional for å håndtere potensielt tomme databaseforespørsler
+import java.util.Optional; // Importerer JpaRepository for å håndtere databaseoperasjoner
+
+import org.springframework.data.jpa.repository.JpaRepository; // Importerer User-modellen som representerer en rad i databasen
+
+import com.g1pro1000.greenCode.model.User; // Importerer Optional for å håndtere potensielt tomme databaseforespørsler
 
 /**
  * UserRepository er ansvarlig for databaseoperasjoner relatert til User-entiteten.
  * Denne grensesnittet utvider JpaRepository, noe som gir tilgang til CRUD-operasjoner 
  * (Create, Read, Update, Delete) uten behov for å skrive SQL selv.
  */
+
 public interface UserRepository extends JpaRepository<User, Long> { // JpaRepository håndterer databaseoperasjoner for User-tabellen
 
     /**
